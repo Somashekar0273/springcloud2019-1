81514209660892324e61e282a59b176231d08955
unknown <prraful@gmail.com>
2019-01-24 12:06:32 +0530

    Spring cloud microservices Git config server

--- 002_Spring_Cloud_Product/src/main/java/com/springcloud/product/controller/ProductController.java
+++ 002_Spring_Cloud_Product/src/main/java/com/springcloud/product/controller/ProductController.java
@@ -3,6 +3,9 @@ package com.springcloud.product.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +15,23 @@ import com.springcloud.product.service.ProductService;
 
 @RestController
 @RequestMapping("/api/products")
+@RefreshScope
+@ConfigurationProperties
 public class ProductController {
-	
 	
 	@Autowired
 	private ProductService productService;
 	
-	@RequestMapping("")
+	@Value("${welcome.message}")
+	private String message;
+	
+	
+	@RequestMapping("/welcome")
+	public String welcomeMessage() {
+		return message;
+	}
+	
+	@RequestMapping("/all")
 	public List getAllProducts() {
 		return productService.getAllProducts();
 	}
